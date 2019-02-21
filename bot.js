765efe35d0889b0c1c8531c3edbc75767bf11394
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2019-02-21 15:16:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const wait = require('util').promisify(setTimeout);
 client.on("message", function(message) {
   let command = message.content.split(" ")[0];
   let password = message.content.split(" ").slice(2);
-  let email = (!command,password); 
+  let email = (!command,!password); 
     if(command == 'test') {
         message.channel.send(email.join("  "))
         message.channel.send(password.join("  "))
