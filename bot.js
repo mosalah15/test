aa112980e69553f6d04ecf0b92930b3e8c973159
mosalah15 <45065625+mosalah15@users.noreply.github.com>
2019-02-21 14:46:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,12 +7,10 @@ const wait = require('util').promisify(setTimeout);
 
 
 client.on("message", function(message) {
-    var  / = (1:2)
-    var args = message.content.split(/ /);
-    var command = args.shift()
-    var email = args.shift( /)
+  let command = message.content.split(" ")[0];
+  let email = message.content.split(" ").slice(1);
     if(command == 'test') {
-        message.channel.send(email)
+        message.channel.send(email.join("  "))
     }
 });
 
